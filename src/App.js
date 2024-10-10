5df87d79b1383b015618c1a71641f362ff27e471
vxnam04 <vnam79475@gmail.com>
2024-10-10 15:38:22 +0700

    secondcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 
 import './App.css';
-import { Menu } from "./index";
+import React from 'react';
+import Home from './Components/page/Home';
+import Baidang from './Components/page/Baidang';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from "antd";
 
 function App() {
   return (
-    <div className='App'>Navbar</div>
-  );
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <Menu items={[
+        { lable: "/Trang chủ" },
+        { lable: "/Tìm kiếm" },
+        { lable: "/Bài đăng" },
+        { lable: "/Thông báo" },
+        { lable: "/Tạo bài viết" },
+        { lable: "/Thể loại" },
+      ]}></Menu>
+      <Content />
+    </div>
+  )
+  function Content() {
+    return <div>
+      <Routes>
+        <Route path="/Trang chủ" element={<Home />} />
+        <Route path="//Tìm kiếm" element={<Baidang />} />
+        <Route path="/Bài đăng" element={<Baidang />} />
+        <Route path="/Thông báo" element={<Baidang />} />
+        <Route path="/Tạo bài viết" element={<Baidang />} />
+        <Route path="/Thể loại" element={<Baidang />} />
+      </Routes>
+    </div>
+  }
 }
 
 export default App;
---
+++ src/Components/page/Baidang.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Baidang = () => {
+    return (
+        <div>
+            Learn how to make a responsive navbar in React using React Router. The navbar shows the active page, highlights links on hover, and sticks to the top of the page on scrolling. This project is a great introduction to the basics of routing in react using React Router.
+
+        </div>
+    )
+}
+
+export default Baidang
